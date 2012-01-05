4a1701b7ecc437dcddb744685edf08b75959a880
Samuel Levy <sam@samuellevy.com>
2012-01-05 01:16:12 +0000

    Menu and tempalte updates

--- common/menus.js
+++ common/menus.js
@@ -1,7 +1,7 @@
 function build_menu(m) {
     var page = $(document).attr('pathname');
     var ul = '<ul>';
-    for (var i = 0; i < m.lenght; i++) {
+    for (var i = 0; i < m.length; i++) {
         var a = (m[i].file == page ? ' class="active"' : '');
         ul += '<li'+a+'><a href="'+m[i].file+'">'+m[i].title+'</a></li>';
     }
@@ -14,4 +14,6 @@ main_menu = [
     { file:'index.html', title:'Home' }
 ];
 
-$('#mainmenu').html(build_menu(main_menu));
+$(document).ready(function() {
+    $('#mainmenu').html(build_menu(main_menu));
+});
